07007fbd27f3cb9207dcd52e29cd1bd17d7a798f
Igor Trentini <igortrentini.2004@gmail.com>
2022-05-23 16:39:36 -0300

    wip: Tratando parametro

--- vio-back/controllers/ocr.go
+++ vio-back/controllers/ocr.go
@@ -3,7 +3,6 @@ package controllers
 import (
 	"fmt"
 	"vio-back/api/ocr"
-	"vio-back/models"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,20 +16,39 @@ func Links(ocrService ocr.Service) OCRController {
 }
 
 func (co OCRController) ToText(c *gin.Context) {
-	var b64 []models.ImgB64
-	if err := c.ShouldBindJSON(&b64); err != nil {
-		c.JSON(500, "deu ruim")
-		fmt.Printf("erro: %s", err)
-		return
-	}
+	first := c.Query("first")
+	second := c.Query("second")
 
-	toStruct, err := co.ocrService.ConvertBase64ToStruct(b64)
-	if err != nil {
-		c.JSON(500, "deu ruim")
-		return
-	}
+	fmt.Println(first)
+	fmt.Println(second)
 
-	fmt.Print(toStruct)
+	// limpar informações
+	// gravar em um arquivo .txt
+	// armazenar em uma pasta no S3
 
 	c.JSON(200, "deu boa")
 }
+
+// fileExample := []byte(first + second)
+// now := time.Now().Format("2006121545")
+// fileName := fmt.Sprintf("paramtest_%s.txt", now)
+
+// err := ioutil.WriteFile("./"+fileName, fileExample, 0644)
+// if err != nil {
+// 	fmt.Print("error")
+// }
+
+// var b64 []models.ImgB64
+// if err := c.ShouldBindJson(&b64); err != nil {
+// 	fmt.Printf("erro: %s", err)
+// 	c.JSON(500, "deu ruim")
+// 	return
+// }
+
+// toStruct, err := co.ocrService.ConvertBase64ToStruct(b64)
+// if err != nil {
+// 	c.JSON(500, "deu ruim")
+// 	return
+// }
+
+// fmt.Print(toStruct)
